ec2a45a18f15036fb955ba7f4a75f888112ddf18
julia <juliasagazcorrea2004@gmail.com>
2024-05-07 16:25:36 -0300

    telalogin

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignIn from "./src/screens/SignIn";
+import SignUp from "./src/screens/SignUp";
+import { Provider } from "./src/context/authContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+const Stack = createNativeStackNavigator();
+const App = () => {
+ return (
+ <NavigationContainer>
+ <Stack.Navigator screenOptions={{ headerShown: false }}>
+ <Stack.Screen name="SignIn" component={SignIn} />
+ <Stack.Screen name="SignUp" component={SignUp} />
+ </Stack.Navigator>
+ </NavigationContainer>
+ );
+};
+export default () => {
+ return (
+ <Provider>
+ <SafeAreaProvider>
+ <App />
+ </SafeAreaProvider>
+ </Provider>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignUp.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text, TextInput, Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+const SignUp = ({ navigation }) => {
+ const [name, setName] = useState("");
+ const [text, setText] = useState("");
+ const [password, setPassword] = useState("");
+ const [showPassword, setShowPassword] = useState(true);
+ return (
+ <SafeAreaView style={styles.container}>
+ <Text style={styles.createAccount}>Criar conta</Text>
+ <TextInput
+ style={styles.textInput}
+ label="Nome"
+ mode="flat"
+ left={<TextInput.Icon icon="account" size={25} color="black" />}
+ value={name}
+ onChangeText={(text) => setName(text)}
+ />
+ <TextInput
+ style={styles.textInput}
+ label="E-mail"
+ mode="flat"
+ left={<TextInput.Icon icon="at" size={25} color="black" />}
+ value={text}
+ onChangeText={(text) => setText(text)}
+ />
+ <TextInput
+ style={styles.textInput}
+ label="Senha"
+ mode="flat"
+ secureTextEntry={showPassword}
+ left={<TextInput.Icon icon="lock" size={25} color="black" />}
+ right={
+ showPassword ? (
+ <TextInput.Icon
+ icon="eye"
+ size={25}
+ color="black"
+ onPress={() => setShowPassword(!showPassword)}
+ />
+ ) : (
+ <TextInput.Icon
+ icon="eye-off"
+ size={25}
+ color="black"
+ onPress={() => setShowPassword(!showPassword)}
+ />
+ )
+ }
+ value={password}
+ onChangeText={(text) => setPassword(text)}
+ />
+ <Button style={styles.createButton} mode="contained">
+ Criar
+ </Button>
+ <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+ <Text>
+ Já tem uma conta? <Text style={styles.loginText}>Faça o login</Text>
+ </Text>
+ </TouchableOpacity>
+ </SafeAreaView>
+ );
+};
+const styles = StyleSheet.create({
+ container: {
+ marginTop: "40%",
+ alignSelf: "center",
+ width: "80%",
+ },
+ createAccount: {
+ fontSize: 40,
+ fontWeight: "bold",
+ marginBottom: 10,
+ },
+ textInput: {
+ marginBottom: 10,
+ },
+ createButton: {
+ padding: 5,
+ margin: 20,
+ width: "50%",
+ alignSelf: "center",
+ },
+ loginText: {
+ fontWeight: "bold",
+ color: "#6200ee",
+ },
+});
+export default SignUp;
